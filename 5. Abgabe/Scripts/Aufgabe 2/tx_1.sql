dc295c22606eaf1ab4397c12389e8b2edad0192d
Florian Natterer <natterer.flo99@gmail.com>
2021-12-08 12:30:31 +0100

    tx_1.sql für Abgabe5-2 begonnen.

---
+++ 5. Abgabe/Scripts/Aufgabe 2/tx_1.sql
@@ -0,0 +1,12 @@
+SET TRANSACTION NAME = 'FeWoInsertion';
+    INSERT INTO Ferienwohnungen(WohnungsID, Zimmerzahl, Groesse, Tagespreis, AdressID)
+        VALUES()
+    INSERT INTO Zusatzaustattungen();
+
+    INSERT INTO bietet(WohnungsID, Ausstattungsbeschreibung)
+        VALUES(999, 'Garten' ) UNION
+        VALUES(999, 'Whirlpool')
+
+
+
+COMMIT FeWoInsertion;
